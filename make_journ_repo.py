87b17710c5cca70ee43885ab8d6d27cdcf286cb7
Clyde Icuspit <cdicuspi@us.ibm.com>
2017-04-18 17:02:15 -0400

    Created a general setup script

--- make_journ_repo.py
+++ make_journ_repo.py
@@ -26,6 +26,34 @@ def make_github_issue(session, url, title, body=None, assignees=None, milestone=
     else:
         print 'Could not create Issue "%s"' % title
         print 'Response:', r.content
+
+def add_collabo(session, url, user, perm ):
+    '''Add collaborators to the repo'''
+
+    # Create the url
+    collabo_url = '%s/%s' % ( url, user )
+
+    # Create the parameter
+    perms = {'permission': perm }
+    # Add the collaborator to our repository
+    r = session.put(collabo_url, json.dumps(perms))
+    if r.status_code == 204:
+        print 'Successfully added collaborator "%s"' % user
+    else:
+        print 'Could not add collaborator "%s"' % user
+        print 'Response:', r.content
+
+def make_milestone(session, url, milestone ):
+    '''Create milestones in the repo'''
+
+    # Add the milestone to our repository
+    r = session.post(url, json.dumps(milestone))
+    if r.status_code == 201:
+        print 'Successfully created milestone "%s"' % milestone['title']
+    else:
+        print 'Could not create milestone "%s"' % milestone['title']
+        print 'Response:', r.content
+
 def usage():
     print '%s -f JSON_file_name -l label' % sys.argv[0]
 
@@ -53,11 +81,22 @@ def main(argv):
 
     # Our url to create issues via POST
     url = '%s/%s/%s/issues' % (data['apiurl'],data['org'], data['repo'])
+
+    # URL to add collaborators
+    collabo_url = '%s/%s/%s/collaborators' % (data['apiurl'],data['org'], data['repo'])
+
+    # URL to create milestones
+    milestone_url = '%s/%s/%s/milestones' % (data['apiurl'],data['org'], data['repo'])
+
     # Create an authenticated session to create the issue
     session = requests.session()
     session.auth=(data['username'], data['token'])
 
+    for user in data["collabo"]:
+      add_collabo(session, collabo_url, user['username'], user['permission'])
 
+    for milestone in data["milestones"]:
+      make_milestone(session, milestone_url, milestone)
 
     for issue in data['issues']:
         make_github_issue(session, url, issue['title'], issue['body'], issue['assignees'], issue['milestone'], [LABEL])
